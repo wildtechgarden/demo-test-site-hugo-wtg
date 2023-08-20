e171fe43e8c91f2c38376537bd4a37fccfd2a8af
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-08-20 04:45:39 -0400

    Update deployment script to correct website
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -5,9 +5,9 @@ set -o pipefail
 
 rm -rf public
 
-URL="https://www.minimal-test-theme.wtg-demos.ca/"
+URL="https://www.wtg-demos.ca/"
 export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
 export BASEURL="$URL"
 
 HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $BASEURL --source exampleSite --destination $(pwd)/public
-rclone sync --progress public/ minimal-test-theme:./
+rclone sync --progress public/ wtgdemos:./
