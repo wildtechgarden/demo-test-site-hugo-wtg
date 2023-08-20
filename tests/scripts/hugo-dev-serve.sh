518cbe8448f71ab5d96e3f9f017632982c184b3b
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-08-20 15:43:17 -0400

    Ensure non-tmp cachedir for Hugo
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/hugo-dev-serve.sh
+++ tests/scripts/hugo-dev-serve.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "${HUGO_CACHEDIR}" ]; then
+	HUGO_CACHEDIR="$(pwd)/hugo-cache"
+fi
+
 export HUGO_MODULE_REPLACEMENTS="github.com/wildtechgarden/minimal-test-theme-hugo-wtg -> ../../minimal-test-theme-hugo-wtg"
 export HUGO_RESOURCEDIR="$(pwd)/resources"
 hugo  serve --buildDrafts --buildFuture --source $(pwd) --environment "development" --config "$(pwd)"/hugo.toml --destination $(pwd)/public
--- tests/scripts/hugo-serve.sh
+++ tests/scripts/hugo-serve.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -z "${HUGO_CACHEDIR}" ]; then
+	HUGO_CACHEDIR="$(pwd)/hugo-cache"
+fi
+
 export HUGO_RESOURCEDIR="$(pwd)/resources"
 hugo  serve --buildDrafts --buildFuture --source $(pwd) --environment "development" --config "$(pwd)"/hugo.toml --destination $(pwd)/public
