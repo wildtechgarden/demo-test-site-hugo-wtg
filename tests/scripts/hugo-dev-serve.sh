d6114aca7347baecc535ffdcca51361a08484edc
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-09-10 08:05:54 -0400

    Add replacements for audit module for dev mode
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/hugo-dev-serve.sh
+++ tests/scripts/hugo-dev-serve.sh
@@ -4,7 +4,7 @@ if [ -z "${HUGO_CACHEDIR}" ]; then
 	HUGO_CACHEDIR="$(pwd)/hugo-cache"
 fi
 
-export HUGO_MODULE_REPLACEMENTS="github.com/wildtechgarden/wild-theme-shell-mod-hugo -> $(pwd)/../wild-theme-shell-mod-hugo, github.com/wildtechgarden/a-wild-theme-mod-hugo -> $(pwd)/../a-wild-theme-mod-hugo, github.com/wildtechgarden/demo-test-site-hugo-wtg -> $(pwd)/../wild-demo-test-site-hugo-wtg, github.com/wildtechgarden/module-starter-hugo-wtg -> $(pwd)/../module-starter-hugo-wtg, github.com/wildtechgarden/minimal-test-theme-hugo-wtg -> $(pwd)/../minimal-test-theme-hugo-wtg"
+export HUGO_MODULE_REPLACEMENTS="github.com/wildtechgarden/wild-theme-shell-mod-hugo -> $(pwd)/../wild-theme-shell-mod-hugo, github.com/wildtechgarden/a-wild-theme-mod-hugo -> $(pwd)/../a-wild-theme-mod-hugo, github.com/wildtechgarden/demo-test-site-hugo-wtg -> $(pwd)/../wild-demo-test-site-hugo-wtg, github.com/wildtechgarden/module-starter-hugo-wtg -> $(pwd)/../module-starter-hugo-wtg, github.com/wildtechgarden/minimal-test-theme-hugo-wtg -> $(pwd)/../minimal-test-theme-hugo-wtg, github.com/wildtechgarden/audit-build-action-hugo -> $(pwd)/../audit-build-action-hugo"
 export HUGO_RESOURCEDIR="$(pwd)/resources"
 export SITEROOT="$(pwd)"
 export SITECONFIG="$(pwd)"/hugo.toml,"$(pwd)"/site.toml
